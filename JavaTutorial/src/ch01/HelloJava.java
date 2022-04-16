853e8cd9b3a70d0bbde0502c661c9cec2fb24aab
Walkers15 <qortmd7777@gmail.com>
2022-04-16 17:48:17 +0900

    add: 패캠 자바강의 시작

---
+++ JavaTutorial/src/ch01/HelloJava.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class HelloJava {
+    /**
+     * 메인 실행 메소드!
+     * @param args 안써도됨
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+    }
+}
